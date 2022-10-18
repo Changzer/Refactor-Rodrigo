ce4c8438688a5b074aa7f126a97635597bf4b1c8
changzer <changzer@gmail.com>
2022-10-18 20:19:45 -0300

    50% de codigo reduzido/otimizado

--- refactor.js
+++ refactor.js
@@ -1,23 +1,14 @@
 function returnRandomNumber() {
-  return Math.round(Math.random() * 100);
-}
+  return Math.round(Math.random() * 100);}
 
 function sumToRandomNumber(num) {
-
-  return `Seu número é ${returnRandomNumber() + num}!`;
-}
+  return `Seu número é ${returnRandomNumber() + num}!`;}
 
 function subtractRandomNumber(num) {
-
-  return `Seu número é ${returnRandomNumber() - num}!`;
-}
+  return `Seu número é ${returnRandomNumber() - num}!`;}
 
 function multiplyToRandomNumber(num) {
-
-  return `Seu número é ${returnRandomNumber() * num}!`;
-}
+  return `Seu número é ${returnRandomNumber() * num}!`;}
 
 function divideRandomNumber(num) {
-
-  return `Seu número é ${returnRandomNumber() / num}!`;
-}
+  return `Seu número é ${returnRandomNumber() / num}!`;}
